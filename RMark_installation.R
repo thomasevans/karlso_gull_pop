0ddd6e1a97fcb427bd59634361cefd160a69159f
Tom Evans <thomas.jude.evans@gmail.com>
2014-05-10 13:59:03 +0200

    File to install RMark

---
+++ RMark_installation.R
@@ -0,0 +1,11 @@
+# Install RMark
+
+# First install the Windows program 'MARK'
+# Download and install from:
+# http://warnercnr.colostate.edu/~gwhite/mark/mark.htm#Downloading
+
+# Install package
+install.packages("RMark")
+
+# Test that package is installed by loading package
+library("RMark")

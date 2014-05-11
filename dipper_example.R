fbd6d76d7df22ac52f558e81838800be4e91311f
Tom Evans <thomas.jude.evans@gmail.com>
2014-05-11 22:17:03 +0200

    More example code, mainly looking at 'mark' sub-functions

--- dipper_example.R
+++ dipper_example.R
@@ -43,3 +43,75 @@ cleanup(ask = FALSE)
 # Confirm that files with 'mark002...' are removed
 list.files()
 
+# Let's remove the first example too
+rm(mark_ex_01)
+cleanup(ask = FALSE)
+
+
+
+# Re-run first example
+mark_ex_01 <- mark(dipper)
+
+# View the PIM (Parameter Index Matrices)
+# Phi
+PIMS(mark_ex_01, "Phi", simplified = FALSE)
+
+# p (case sensitive! Lower-case 'p' not 'P')
+PIMS(mark_ex_01, "p", simplified = FALSE)
+
+
+
+# Adding grouping variables/ and further variables (e.g. age, sex, etc)
+# This is possible, see further down for how!
+
+# View help for 'mark' function
+?mark
+# many different potential arguments. E.g. for model type ('model' argument)
+
+
+
+# 'mark' function works by sequentially calling 5 component functions:
+# 1. process.data
+# 2. make.design.data
+# 3. make.mark.model
+# 4. run.mark.model
+# 5. summary.mark
+
+# 1. process.data example (normally you would just the 'mark' function, not the
+# component functions, this is for illustration only)
+dipper.process <- process.data(dipper, model = "CJS",
+                               begin.time = 1980,
+                               time.intervals = c(1,.5,1,.75,.25,1),
+                               groups = "sex")
+
+# Resulting object is list with various components
+str(dipper.process)
+# Not all variables are used in all model types, therefor they are given
+# null values (NULL)
+
+# Can extact specific components
+dipper.process$group.covariates
+
+# e.g. for time intervals
+dipper.process$time.intervals
+
+
+# 2. make.design.data
+
+# First re-process the dipper data
+dipper.process <- process.data(dipper,
+                               model = "CJS",
+                               begin.time = 1980,
+                               groups = "sex")
+
+# Run make.design.data
+dipper.ddl <- make.design.data(dipper.process)
+# View structure
+str(dipper.ddl)
+
+# Now when 'mark' function is ran it skips the first two steps, using the
+# already processed data (output from 'make.design.data') 
+dipper.mark.ex.1 <- mark(dipper.process, dipper.ddl)
+
+
+PIMS(dipper.mark.ex.1, "Phi", simplified = FALSE)
